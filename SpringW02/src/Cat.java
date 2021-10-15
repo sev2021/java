195c33e221b73c9d3481e829d8b8ac74484e6520
sev2021 <seweryn.sapinski@gmail.com>
2021-10-15 02:20:55 +0100

    class worm-up

--- SpringW02/src/Cat.java
+++ SpringW02/src/Cat.java
@@ -1,27 +1,19 @@
 public class Cat {
-	
-	String name = "Sam", color = "black";
-	int age = 3;
-	Integer date = 2021; //Integer class is wrapper for primitive
-	
-	
-	public Cat() { //constructor, can't declare but can change
-		name = "SamSam";
-		age = 33;
+		double weight;
+		String color;
+	public Cat() {
+		weight = 1.5;
+		color = "yello";
 	}
-
-	public void chase() {  // Cat class method
-		System.out.println("I am only ");
-	} // Cat class end
 	
+	public void eat() {
+		weight += 0.5;
+	}
 	
-	public static void main(String[] args) {
-		Dog hotDog = new Dog(); // Dog class from file Dog.java
-		hotDog.name = "Burito";
-		hotDog.bark();
-
-		Cat hotCat = new Cat(); // Local variables, weird photos
-		hotCat.chase();
-		System.out.println(hotCat.date.toString());
+	public static void main(String args[]) {
+		System.out.println("PPRPRPRON");
+		Cat myCat = new Cat();
+		myCat.eat();
+		System.out.println(myCat.weight);
 	}
 }
