4c63abd6b7eb894f45f3e3a4fb219f7a13e2af0f
sev2021 <seweryn.sapinski@gmail.com>
2021-10-16 02:50:46 +0100

    first instantiable

--- SpringW02/src/Cat.java
+++ SpringW02/src/Cat.java
@@ -1,19 +1,23 @@
+import javax.swing.JOptionPane;
+
 public class Cat {
-		double weight;
-		String color;
-	public Cat() {
-		weight = 1.5;
-		color = "yello";
-	}
-	
-	public void eat() {
-		weight += 0.5;
-	}
-	
 	public static void main(String args[]) {
-		System.out.println("PPRPRPRON");
-		Cat myCat = new Cat();
-		myCat.eat();
-		System.out.println(myCat.weight);
+		int x, y, ans;
+		
+		x = Integer.parseInt(JOptionPane.showInputDialog("Enter number:"));
+		y = Integer.parseInt(JOptionPane.showInputDialog("Enter number:"));
+		
+		//create object from class Cat_Instantiable
+		Cat_Instantiable myAdd = new Cat_Instantiable();
+		
+		//send variables to main class
+		myAdd.setX(x);
+		myAdd.setY(y);
+		
+		//run calculation in main class
+		myAdd.computeAns();
+		
+		//display results
+		System.out.println("Sum of " + x + " + " + y + " = " + myAdd.getAns());
 	}
 }
