844cfc7e7ce363ad7f41bfdaf5ad288186a45542
sev2021 <seweryn.sapinski@gmail.com>
2021-10-28 02:08:44 +0100

    Lesson example my

---
+++ SpringW05L0/src/CinemaProblem.java
@@ -0,0 +1,39 @@
+public class CinemaProblem {
+
+	//declare vars
+	private String username = "", message = "";
+	private int userage = 0;
+	//constructor
+	public CinemaProblem() {
+	}
+	
+	//// INPUT
+    //set method
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public void setUserAge(int userage) {
+		this.userage = userage;
+	}
+	
+	//// PROCESS
+	//compute
+	public void computeMessage() {
+		if(userage >= 18 ) {
+			if(username.equals("Sam")) {
+				message = "Welcome, Sam!";
+			}
+			else {
+				message = "Sorry, wrong username.";
+			}
+		}
+		else {
+			message = "You must be over 18.";
+		}
+	}
+	
+	//get method
+	public String getMessage() {
+		return message;
+	}
+}
---
+++ SpringW05L0/src/CinemaProblemApp.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+
+public class CinemaProblemApp {
+		public static void main(String[] args) {
+			//declare vars
+			String username, message;
+			int age;
+			//declare and create objects
+			CinemaProblem myCinema = new CinemaProblem();
+			
+			//// INPUT
+			username = JOptionPane.showInputDialog("What is your name?");
+			age = Integer.parseInt (JOptionPane.showInputDialog("What is yout age?"));
+			
+			//// PROCESS
+			//set
+			myCinema.setUsername(username);
+			myCinema.setUserAge(age) ;
+			//compute
+			myCinema.computeMessage();
+			//get
+			message = myCinema.getMessage();
+			
+			//// OUTPUT
+			//output
+			JOptionPane.showMessageDialog(null, message);
+			
+			
+			
+			
+			
+			
+		}
+		
+}
