4a3f5a60afa9c497b897ab277610debc22ca5953
sev2021 <seweryn.sapinski@gmail.com>
2021-10-31 01:00:22 +0100

    Dona and working

--- SpringW05L2/src/Hotel.java
+++ SpringW05L2/src/Hotel.java
@@ -1,4 +1,39 @@
-
 public class Hotel {
+	////INPUT
+	//declare vars
+	private String msg;
+	private int userAge, numNights;
 
+	//constructor
+	public Hotel() {
+		msg = "";
+		userAge = 0;
+		numNights = 0;
+	}
+		
+	//set method
+	public void setUserAge(int userage) {
+		this.userAge = userage;
+	}
+	public void  setNumNights(int numNights) {
+		this.numNights = numNights;
+	}
+	
+	////PROCESS
+	//compute
+	public void computeTotalPrice() {
+		if(userAge < 18) {
+			msg = "You have to be over 18 to book a room.";
+		}
+		else {
+			msg = "Total price for " + numNights + " nights is " + numNights * 90 + " Euro.";
+		}
+	}
+	
+	////OUTPUT
+	//get method
+	public String getMessage() {
+		return msg;
+	}
+	
 }
--- SpringW05L2/src/HotelApp.java
+++ SpringW05L2/src/HotelApp.java
@@ -1,4 +1,32 @@
-
+import javax.swing.JOptionPane;
 public class HotelApp {
-
+	public static void main(String[] args) {
+		////INPUT
+		//declare vars
+		String userName, confMessage;
+		int userAge, numNights;
+		
+		//declare and create object
+		Hotel bookHotel = new Hotel();
+		userName = JOptionPane.showInputDialog("What is your name?");
+		numNights = Integer.parseInt(JOptionPane.showInputDialog("How many nights you would like to book?"));
+		userAge = Integer.parseInt(JOptionPane.showInputDialog("What is your age?"));
+		
+		
+		////PROCESS
+		//set
+		bookHotel.setNumNights(numNights);
+		bookHotel.setUserAge(userAge);
+		
+		//compute
+		bookHotel.computeTotalPrice();
+		
+		//get
+		confMessage = bookHotel.getMessage();
+		
+		
+		////OUTPUT
+		JOptionPane.showMessageDialog(null, "Dear " + userName + ".\n" + confMessage);
+		
+	}
 }
