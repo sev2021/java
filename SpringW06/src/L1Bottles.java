d60444ede316714e82c73bb79b5304778d490ef5
sev2021 <seweryn.sapinski@gmail.com>
2021-11-04 03:02:36 +0000

    99 bottles

---
+++ SpringW06/src/L1Bottles.java
@@ -0,0 +1,22 @@
+/* 99 bottles of beer on the wall, 99 bottles of beer
+/* Take one down and pass it around, 98 bottles of beer on the wall
+*/
+
+public class L1Bottles {
+	public static void main(String args[]) {
+		for(int i = 99; i > 2; i--) {
+			System.out.println("\n"  + i + " bottles of beer on the wall, " + i + " bottles of beer");
+			System.out.println("Take one down and pass it around, " +  (i -1) + " bottles of beer on the wall");
+		}
+		String threeLastVerses = "\n2 bottles of beer on the wall, 2 bottles of beer\n"
+				+ "Take one down and pass it around, 1 bottle of beer on the wall\n"
+				+ "\n"
+				+ "1 bottle of beer on the wall, 1 bottle of beer\n"
+				+ "Take one down and pass it around, no more bottles of beer on the wall\n"
+				+ "\n"
+				+ "No more bottles of beer on the wall, no more bottles of beer\n"
+				+ "Go to the store and buy some more, 99 bottles of beer on the wall";
+
+		System.out.println(threeLastVerses);
+	}
+}
