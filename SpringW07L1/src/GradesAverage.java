a45115b599df1c228fb5c9485aa1a63040e60f30
sev2021 <seweryn.sapinski@gmail.com>
2021-11-13 14:37:20 +0000

    pass to laptop

--- SpringW07L1/src/GradesAverage.java
+++ SpringW07L1/src/GradesAverage.java
@@ -1,4 +1,31 @@
-
 public class GradesAverage {
-
+	////INPUT
+	//declare variables
+	private int subjectsNumber, subjectGrade, finalGradesAverage;
+	//private int[] subjectsArray;
+	//constructor
+	public GradesAverage() {}
+	
+	//setter class
+	public void  setSubjectsNumber(int subjectsNumber) {
+		this.subjectsNumber = subjectsNumber;
+	}
+	private int[] subjectsArray = new int[subjectsNumber];
+	public void setSubjectsArray(int subjectGrade, int key) {
+		this.subjectsArray[key] = subjectGrade;
+	}
+	
+	////PROCESS 
+	//compute
+	public void 
+	
+	////
+	//getters classes
+	public int[] getSubjectsArray() {
+		return subjectsArray; 
+	}
+	public int getFinalGradesAverage() {
+		return int finalGradesAverage;
+	}
+	
 }
--- SpringW07L1/src/GradesAverageApp.java
+++ SpringW07L1/src/GradesAverageApp.java
@@ -1,4 +1,21 @@
-
+import javax.swing.JOptionPane;
 public class GradesAverageApp {
-
+	public static void main(String[] agrs) {
+		////INTPUT
+		//declare variables
+		int subjectsNumber, subjectGrade;
+		//create objects
+		GradesAverage calcMyGrades = new GradesAvegrage();
+		
+		//set
+		calcMyGrades.setSubjectsNumber(subjectsNumber);
+		
+		////PROCESS
+		//compute
+		
+		
+		
+		////OUTPUP
+		//get
+	}
 }
