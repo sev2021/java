003e74066d33d41135fbd4d716c20d74b6097e37
sev2021 <seweryn.sapinski@gmail.com>
2021-11-14 02:33:02 +0000

    Final working!

--- SpringW07L1/src/GradesAverage.java
+++ SpringW07L1/src/GradesAverage.java
@@ -1,31 +1,37 @@
 public class GradesAverage {
 	////INPUT
 	//declare variables
-	private int subjectsNumber, subjectGrade, finalGradesAverage;
-	//private int[] subjectsArray;
+	private int subjectsNumber, subjectGrade, finalGradesAverage = 0;
+	private String finalGradesList = "";
+	private int[] subjectsArray;
 	//constructor
 	public GradesAverage() {}
-	
 	//setter class
 	public void  setSubjectsNumber(int subjectsNumber) {
 		this.subjectsNumber = subjectsNumber;
+		this.subjectsArray = new int[subjectsNumber];
 	}
-	private int[] subjectsArray = new int[subjectsNumber];
-	public void setSubjectsArray(int subjectGrade, int key) {
-		this.subjectsArray[key] = subjectGrade;
-	}
-	
-	////PROCESS 
+
+	////PROCESS ?
 	//compute
-	public void 
+	public void setSubjectsArray(int subjectGrade, int key) {
+		subjectsArray[key] = subjectGrade;
+	}
 	
-	////
+	////OUTPUT
 	//getters classes
-	public int[] getSubjectsArray() {
-		return subjectsArray; 
-	}
 	public int getFinalGradesAverage() {
-		return int finalGradesAverage;
+		for(int i: subjectsArray) {
+			finalGradesAverage += i;
+		}
+		finalGradesAverage /= subjectsArray.length;
+		return finalGradesAverage;
 	}
 	
+	public String getFinalGradesList() {
+		for(int i=0; i<subjectsArray.length; i++) {
+			finalGradesList += "\nSubject " + (i+1) + " : " + subjectsArray[i];
+		}
+		return finalGradesList;
+	}
 }
--- SpringW07L1/src/GradesAverageApp.java
+++ SpringW07L1/src/GradesAverageApp.java
@@ -3,19 +3,26 @@ public class GradesAverageApp {
 	public static void main(String[] agrs) {
 		////INTPUT
 		//declare variables
-		int subjectsNumber, subjectGrade;
+		int subjectsNumber = 0, subjectGrade;
 		//create objects
-		GradesAverage calcMyGrades = new GradesAvegrage();
+		GradesAverage calcMyGrades = new GradesAverage ();
 		
 		//set
+		while(subjectsNumber == 0) {
+			subjectsNumber = Integer.parseInt(JOptionPane.showInputDialog("How many subjects you have? "));
+		}
 		calcMyGrades.setSubjectsNumber(subjectsNumber);
 		
-		////PROCESS
+		////PROCESS ?
 		//compute
-		
-		
+		for(int i = 0; i < subjectsNumber; i++) {
+			subjectGrade = Integer.parseInt(JOptionPane.showInputDialog("Enter grade for subject " + (i+1) + " : "));
+			calcMyGrades.setSubjectsArray(subjectGrade, i);
+		}
 		
 		////OUTPUP
 		//get
+		JOptionPane.showMessageDialog(null, "Average grade: " + calcMyGrades.getFinalGradesAverage());
+		JOptionPane.showMessageDialog(null, "Your grades: " + calcMyGrades.getFinalGradesList());
 	}
 }
