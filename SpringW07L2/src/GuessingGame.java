4a356e576b6764480fff88783e398ac7432140ad
sev2021 <seweryn.sapinski@gmail.com>
2021-11-15 01:50:14 +0000

    result error fixed

--- SpringW07L2/src/GuessingGame.java
+++ SpringW07L2/src/GuessingGame.java
@@ -33,8 +33,8 @@ public class GuessingGame {
 				stringResult = "You win!";
 				i = guessTries;
 			}
-			stringResult += "\nWinning number: " + computerNumber + ".";
 		}
+		stringResult += "\nWinning number: " + computerNumber + ".";
 	}
 	
 	////OUTPUT
