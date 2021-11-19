62e62c18155083a3b39a964fd0014933dc4ba9f6
sev2021 <seweryn.sapinski@gmail.com>
2021-11-19 02:58:08 +0000

    final today

--- Springw08L2/src/FiveByFour.java
+++ Springw08L2/src/FiveByFour.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class FiveByFour {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		
 		String userNumber = keyboard.next();
 		//System.out.print(Integer.parseInt(userNumber));
 		for(int i =0; i < userNumber.length(); i++) System.out.println(userNumber.charAt(i) + "::" + (int)userNumber.charAt(i));
