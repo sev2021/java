db8afdeda3647fffc1a961728f9bfe5ac0ed7ba6
sev2021 <seweryn.sapinski@gmail.com>
2021-11-20 22:55:30 +0000

    ubuntu

--- Springw08L2/src/FiveByFour.java
+++ Springw08L2/src/FiveByFour.java
@@ -4,7 +4,7 @@ public class FiveByFour {
 		Scanner keyboard = new Scanner(System.in);
 		String userNumber = keyboard.next();
 		
-		//System.out.print(Integer.parseInt(userNumber));
+		//System.out.print(Integer.parseInt(userNumber));GIT
 		for(int i =0; i < userNumber.length(); i++) System.out.println(userNumber.charAt(i) + "::" + (int)userNumber.charAt(i));
 	}
 }
