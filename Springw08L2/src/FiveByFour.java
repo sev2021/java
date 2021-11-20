fdee95076296a46aaffafbf818f5f608e23ac723
sev2021 <seweryn.sapinski@gmail.com>
2021-11-20 22:57:56 +0000

    more here

--- Springw08L2/src/FiveByFour.java
+++ Springw08L2/src/FiveByFour.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 public class FiveByFour {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
-		String userNumber = keyboard.next();
+		String nextResult = "n", userNumber = "";
 		
-		//System.out.print(Integer.parseInt(userNumber));
+		do {
+		System.out.print("Enter string: ");
+		userNumber = keyboard.next();
+		System.out.print("More? (Y/N) ");
+		nextResult = keyboard.next();		
+		} while (nextResult.equals("Y"));
+		
 		for(int i =0; i < userNumber.length(); i++) System.out.println(userNumber.charAt(i) + "::" + (int)userNumber.charAt(i));
 	}
 }
