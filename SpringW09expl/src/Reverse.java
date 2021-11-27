f56b10b5c62352285417d2361501974e92c48826
sev2021 <seweryn.sapinski@gmail.com>
2021-11-27 17:49:22 +0000

    Final String Reverse

--- SpringW09expl/src/Reverse.java
+++ SpringW09expl/src/Reverse.java
@@ -1,4 +1,26 @@
 public class Reverse {
 	////INPUT
 	//variable declaration
+	private String userInput, reversed, reversedMy = "";
+	private StringBuffer strBuff = new StringBuffer();
+	
+	public Reverse(String userInput) {
+		this.userInput = userInput;
+	}
+	
+	public void doReverse() {
+		//userInput += userInput;
+		for(int i = userInput.length() - 1; i>=0; i-- ) {
+			strBuff.append(userInput.charAt(i));
+			reversedMy +=userInput.charAt(i);
+		}
+		reversed = strBuff.toString();
+	}
+	
+	public String getReverse() {
+		return reversed;
+	}
+	public String getReverseMy() {
+		return reversedMy;
+	}
 }
--- SpringW09expl/src/ReverseApp.java
+++ SpringW09expl/src/ReverseApp.java
@@ -1,4 +1,22 @@
+import javax.swing.JOptionPane;
 public class ReverseApp {
-	////INPUT
-	//variable declaration
+	public static void main(String[] args) {
+		////INPUT
+		//variable declaration
+		String userInput;
+		String reversed;
+		userInput = JOptionPane.showInputDialog("Enter string to reverse:"); 
+		
+		//set methods
+		Reverse stringReverse = new Reverse(userInput);
+	
+		////PROCESS
+		//compute
+		stringReverse.doReverse();
+		
+		////OUTPUT
+		//get method
+		JOptionPane.showMessageDialog(null, "StringBuffer and append: " + stringReverse.getReverse()
+				+"\nString and concatenate: " + stringReverse.getReverseMy());
+	}
 }
