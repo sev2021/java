4d5c1795d7232e2d5451cbcc7d8c1f5edfe0f2b0
sev2021 <seweryn.sapinski@gmail.com>
2021-12-05 19:10:27 +0000

    Task text adeded

---
+++ SpringW10Sam/src/CalcApp.java
@@ -0,0 +1,11 @@
+/* Create an application that takes in 2 numbers and a String from user.
+ * BAsed on the submitted character (a,s,m or d),
+ * the application should output the 2 numbers either added,
+ * subtracted, multiplied or divided.
+ * The application should also take the final amount and return whether it is odd or even.
+ * - This application should have  instantiable classes
+ * - Each instantiable class will only have 1 method that combines set, compute and get
+ */
+public class CalcApp {
+
+}
