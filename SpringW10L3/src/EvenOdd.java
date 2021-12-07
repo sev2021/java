108922441ff98c8e005c76937d4e6aa0603455a9
user <user@x64>
2021-12-07 01:34:17 +0000

    Even/Odd array generator done

--- SpringW10L3/src/EvenOdd.java
+++ SpringW10L3/src/EvenOdd.java
@@ -1,42 +1,40 @@
-import java.util.Arrays;
+
 public class EvenOdd {
 	private String[] stringArray;
 	private int[] intArray;
 	private int[] resultArray;
-	private int evenSize;
+	private int evenSize, arrayMode;
+	private String userInput;
 
+	////set, compute, get
 	public int[] doEvenOdd(String userInputArray, String userInput) {
+		//set strings array split from user's input (string of integers)
 		stringArray = userInputArray.split(",");
+		
+		//compute integers array size based on strings array size
 		intArray = new int[stringArray.length - 2];
-		System.out.println(userInputArray);
-		System.out.println(Arrays.toString(stringArray));
+		
+		//compute integers array parsed from strings array
 		for(int i=0; i<stringArray.length - 2; i++) {
 			intArray[i] = Integer.parseInt(stringArray[i].strip()); 
 			if(intArray[i] % 2 == 0) evenSize ++;
 		}
-		if(userInput.equalsIgnoreCase("0")) { //even Resut
-			resultArray = new int[evenSize];
-			int j = 0;
-			for(int i:intArray) {
-				if(i%2 == 0) {
-					resultArray[j] = i;
-					j++;
-				}
-			}
-		}
 		
-		if(userInput.equalsIgnoreCase("1")) { //odd Resut
-			resultArray = new int[intArray.length - evenSize];
-			int j = 0;
-			for(int i:intArray) {
-				if(i%2 == 1) {
-					resultArray[j] = i;
-					j++;
-				}
-				
+		//set array mode parsed from user's input ("0" or "1")
+		this.userInput = userInput;
+		arrayMode = Integer.parseInt(userInput);
+		
+		//compute result array based on integers array and array mode
+		resultArray = new int[Math.abs(intArray.length * arrayMode - evenSize)];
+		int j = 0;
+		for(int i:intArray) {
+			if(i%2 == arrayMode) {
+				resultArray[j] = i;
+				j++;
 			}
 		}
-		System.out.println(Arrays.toString(resultArray));
+
+		//get(return) result array 
 		return resultArray;
 	}
 }
--- SpringW10L3/src/EvenOddApp.java
+++ SpringW10L3/src/EvenOddApp.java
@@ -1,22 +1,24 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EvenOddApp {
 	public static void main(String[] args) {
-		////SHORT VERSION
-		//declare variable
+		////SHORT VERSION BASED on LATEST SAM'S VIDEO
+		//declare variable, create object
 		String userInput = "", userInputArray = "";
 		Scanner keyboard = new Scanner(System.in);
 		EvenOdd myEO = new EvenOdd();
+		int[] resultArray;
 		
 		////INPUT
-		//set array
+		//set user's input array as string of integers
 		do {
 			System.out.print("Add number to array [" + userInputArray + "], or press Enter to compute: ");
 			userInput = keyboard.nextLine();
 			userInputArray += userInput + ", "; 
 		}while(userInput != "");
 		
-		//set mode
+		//set user's even/odd mode as string "0" or "1"
 		do {
 			System.out.print("Select mode: 0 - to get even numbers, 1 - to get odd numbers: ");
 			userInput = keyboard.next();
@@ -24,6 +26,7 @@ public class EvenOddApp {
 		
 		
 		//set, compute, get
-		myEO.doEvenOdd(userInputArray, userInput);
+		resultArray = myEO.doEvenOdd(userInputArray, userInput);
+		System.out.println(Arrays.toString(resultArray));
 	}
 }
