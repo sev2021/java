6f23b3fdb001bbcd8e1af87f0002fb79adf4abf3
user <user@192.168.0.9>
2021-12-12 16:02:39 +0000

    initial commit

---
+++ SpringW11Sam/src/Animal.java
@@ -0,0 +1,22 @@
+public class Animal {
+	private boolean animalHungry;
+	private String animalSound;
+	
+	public Animal() {
+		animalHungry = true;
+	}
+	
+	public void checkHunger() {
+		if(animalHungry) System.out.println("This animal looks hungry.");
+		else System.out.println("This animal looks satisfied.");
+	}
+	
+	public void feedAnimal() {
+		animalHungry = false;
+		System.out.println("You fed the animal.");
+	}
+	
+	public void animalMakesSound(String animalSound) {
+		System.out.println(animalSound);
+	}
+}
