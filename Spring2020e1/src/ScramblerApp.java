af387e81916c54c64790d450e7ebd301da8ba283
user <user@192.168.0.9>
2021-12-14 23:55:37 +0000

    instruction added

---
+++ Spring2020e1/src/ScramblerApp.java
@@ -0,0 +1,13 @@
+
+public class ScramblerApp {
+	public static void main(String[] args) {
+		////INPUT
+		//declare variable
+		String userText;
+		
+		TextScrambler myScrambler = new TextScrambler();
+		
+		System.out.println(myScrambler.getScrambledTExt());
+		
+	}
+}
