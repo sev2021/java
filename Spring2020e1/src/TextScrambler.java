0488565f52adad90b9a6e0679735b72f6422eac2
user <user@192.168.0.9>
2021-12-14 23:43:48 +0000

    general class done

---
+++ Spring2020e1/src/TextScrambler.java
@@ -0,0 +1,31 @@
+
+public class TextScrambler {
+	////INPUT
+	//declare variable for instantiable class
+	private String userText;
+	
+	//constructor
+	public TextScrambler() {
+		userText = "";
+	}
+	
+	//set method
+	public void setUserText(String userText) {
+		this.userText = userText;
+	}
+	
+	////PROCESS
+	//compute method
+	public void computeScrambledText() {
+		userText = "";
+	}
+	
+	
+	////OUTPUT
+	//get method
+	public String getScrambledTExt() {
+		return userText;
+	}
+
+	
+}
