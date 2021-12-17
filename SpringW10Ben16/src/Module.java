853b0b8bbf2b540ee087de37aa965d7a93408b89
sev2021 <seweryn.sapinski@gmail.com>
2021-12-17 17:06:04 +0000

    initial commit

---
+++ SpringW10Ben16/src/Module.java
@@ -0,0 +1,21 @@
+
+public class Module {
+	//declare variable
+	private String name;
+	private float marks;
+	
+	//constructor
+	public Module() {
+		this.name = "";
+		this.marks = 0.0f;
+	}
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setMarks(float marks) {
+		this.marks = marks;
+	}
+	
+}
---
+++ SpringW10Ben16/src/ModuleApp.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ModuleApp {
+	public static void main(String[] args) {
+		//set variable
+		String userInput;
+		
+		Module[] myMod = new Module[5];
+		
+		//setters
+		for(int i; i<5; i++) {
+			myMod = Module[i];
+			mymod
+		}
+	
+	}
+}
