6094ef671bc42f1ee13c9abb47ece36d3f337514
sev2021 <seweryn.sapinski@gmail.com>
2021-12-17 18:39:40 +0000

    Java v16 final

--- SpringW10Ben16/src/ModuleApp.java
+++ SpringW10Ben16/src/ModuleApp.java
@@ -4,13 +4,22 @@ public class ModuleApp {
 		
 		Module[] moduleArray = new Module[5];
 
-		for(int i=0; i<3; i++) {
-			Module module = new Module();
-			System.out.print("Please enter module name: ");
-			module.setName(new Scanner(System.in).next());
-			System.out.print("Please enter module marks: ");
-			module.setMarks(new Scanner(System.in).nextFloat());	
-			moduleArray[i] = module;
+		
+//		for(int i=0; i<5; i++) {  // ORIGINAL SOLUTION
+//			//Module module = new Module();
+//			
+//			System.out.print("Please enter module name: ");
+//			moduleArray[i].setName(new Scanner(System.in).next());
+//			System.out.print("Please enter module marks: ");
+//			moduleArray[i].setMarks(new Scanner(System.in).nextFloat());	
+//		
+//			moduleArray[i] = new Module();
+//		}
+		
+		for(int i=0; i<5; i++) {  // SHORT SOLUTION WITHOUT EXTRA VARIABLE
+			moduleArray[i] = new Module();
+			moduleArray[i].setName(new Scanner(System.in).next());
+			moduleArray[i].setMarks(new Scanner(System.in).nextFloat());	
 		}
 		
 		for(int i=0; i<3; i++) {
