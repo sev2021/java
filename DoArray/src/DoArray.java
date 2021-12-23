5895e7c95b29b524e0bdf4cbe01f8f80844be52d
sev2021 <seweryn.sapinski@gmail.com>
2021-12-23 17:36:54 +0000

    init commit

---
+++ DoArray/src/DoArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class DoArray {
+	
+	public static void main(String[] args) {
+		String userInput = "", userString = "";
+		String[] userArray;
+		
+		do {
+			userInput = new Scanner(System.in).nextLine();
+			userString += userInput + ",";
+		}
+		while(!userInput.isEmpty());
+			
+		userArray = userString.split(",");
+		
+		System.out.println(Arrays.toString(userArray));
+		for(int i=0; i<userArray.length; i++) {
+			System.out.println(i + "." + userArray[i] + " " + userArray[i].length());
+		}
+	}
+}
