3366f892a6db3d11327191694d32393202795687
sev2021 <seweryn.sapinski@gmail.com>
2021-12-29 03:08:48 +0000

    two string validations

--- SpringER1MPA2/src/TScrambler.java
+++ SpringER1MPA2/src/TScrambler.java
@@ -1,4 +1,56 @@
-
+/* - Task:
+ * Ask the user at the beginning of the application how many paragraphs they would like to scramble, 
+ * and ensure that the application scrambles that amount of sentences. 
+ * This should be completed using a graphical interface such as JOptionPane 
+ * - Input Validation: 
+ * The text must contain at least 2 spaces. . 
+ * Validation should take place in the instantiable class and will halt the running 
+ *  of the application if validation does not pass
+ * - Scrambling:
+ * 	Every character that is not a vowel should be replaced by the last 3 digits of your student number. .
+ •	This should be hard coded into the application and is not required as user input.
+*/
 public class TScrambler {
-
+	////INPUT
+	//declare variable
+	private String[] userInputArray;
+	
+	
+	//constructor
+	public TScrambler() {
+	}
+	
+	//setter method
+	public void setInputArray(String[] userInputArray) {
+		this.userInputArray = userInputArray;
+	}
+	
+	//compute method
+	public void computeArray() {
+//		validation
+		for(String validString: userInputArray) {
+			System.out.println(validString);
+			int charOn = validString.indexOf('a');
+			if( charOn == -1) System.out.println("BAD Validating!");
+			if( validString.indexOf('a', charOn) == -1) System.out.println("BAD Validating!");
+			System.out.println("END Validating!");
+		}
+	}
+		
+//		validation2
+//		for(String validString: userInputArray) {
+//			System.out.println("Validate string:" + validString);
+//			if(validString.length() - validString.replaceAll(" ", "").length() < 2) {
+//				System.out.println("String without at least 2 spaces:" + validString);
+//				System.exit(0);
+//			}
+//		}
+	
+	
+	//getter method
+	public String[] getComputedArray() {
+		return userInputArray;
+	}
+	
+	
 }
--- SpringER1MPA2/src/TScramblerApp.java
+++ SpringER1MPA2/src/TScramblerApp.java
@@ -1,4 +1,44 @@
-
+/* - Task:
+ * Ask the user at the beginning of the application how many paragraphs they would like to scramble, 
+ * and ensure that the application scrambles that amount of sentences. 
+ * This should be completed using a graphical interface such as JOptionPane 
+ * - Input Validation: 
+ * The text must contain at least 2 spaces. . 
+ * Validation should take place in the instantiable class and will halt the running 
+ *  of the application if validation does not pass
+ * - Scrambling:
+ * 	Every character that is not a vowel should be replaced by the last 3 digits of your student number. .
+ •	This should be hard coded into the application and is not required as user input.
+*/
+import java.util.Arrays;
+import javax.swing.JOptionPane;
 public class TScramblerApp {
-
+	public static void main(String[] agrs) {
+		////INPUT
+		//declare variable
+		String userInput;
+		String[] userInputArray;
+		int inputArraySize;
+		//create object
+		TScrambler doScramble = new TScrambler();
+		//set
+		inputArraySize = Integer.parseInt(JOptionPane.showInputDialog("How many paragraphs \nyou would like to scramble?"));
+		userInputArray = new String[inputArraySize];
+		
+		for(int i=0; i<inputArraySize; i++) {
+			userInputArray[i] = JOptionPane.showInputDialog("Please enter " + (i+1) + " paragraph:");
+		}
+		doScramble.setInputArray(userInputArray);
+		
+		////PROCESS
+		//compute
+		doScramble.computeArray();
+		
+		
+		////OUTPUT
+		//get
+		System.out.print(Arrays.toString(doScramble.getComputedArray()));
+		
+		
+	}
 }
