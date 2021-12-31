0e7fcb5b58500ae05bcf2685778d0238bedf0fb7
sev2021 <seweryn.sapinski@gmail.com>
2021-12-31 02:07:59 +0000

    GO BACK

---
+++ SpringER2MPA1/src/TScramblerApp.java
@@ -0,0 +1,18 @@
+/* - Task:
+ * Ask the user to provide a paragraph and after the paragraph is encoded, 
+ * create a popup that asks the user if they would like to encode another paragraph. 
+ * As long as the user clicks “yes” the application repeat. 
+ * When the user clicks “no”, no other sentences are scrambled 
+ * This should be completed using a graphical interface such as JOptionPane 
+ * - Input Validation: 
+ * The text must start with an upper-case letter and end with a full stop. 
+ * Validation should take place in the instantiable class and will halt the running 
+ *  of the application if validation does not pass
+ * - Scrambling:
+ * 	The scrambled text should start with the number of characters in the initial text.
+ *	Replace each vowel with your initials. 
+ *  This should be hard coded into the application and is not required as user input.
+*/
+public class TScramblerApp {
+
+}
