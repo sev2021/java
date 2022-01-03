7395ada04d1741a6a36e8b348ca937d229e3fffa
sev2021 <seweryn.sapinski@gmail.com>
2022-01-03 02:50:52 +0000

    initial

---
+++ HDipExam2020/src/DefPass.java
@@ -0,0 +1,10 @@
+
+public class DefPass {
+	////INPUT
+	//declare variables
+	private 
+	
+	public DeffPass() {
+	}
+	
+}
---
+++ HDipExam2020/src/DefPassApp.java
@@ -0,0 +1,14 @@
+
+public class DefPassApp {
+	public static void main(String[] args) {
+		////INPUT
+		//declare vars and objects
+		
+		DefPass myPass = new DefPass();
+		
+		myPass.setUserInput("Rewe Mtgwetwe", 3);
+		myPass.computeUserInput();
+		myPass.getUserInput();
+		
+	}
+}
