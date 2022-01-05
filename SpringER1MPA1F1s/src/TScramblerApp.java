78dde3aacd83d54b9b2b26f150977372d9a118c4
sev2021 <seweryn.sapinski@gmail.com>
2022-01-05 23:16:00 +0000

    initial commit

---
+++ SpringER1MPA1F1s/src/TScramblerApp.java
@@ -0,0 +1,24 @@
+/*MPA1 (App class)
+Ask the user at the beginning of the application how many paragraphs they would like to scramble, 
+and ensure that the application scrambles that amount of sentences. 
+This should be completed using a graphical interface such as JOptionPane.
+* ER1  (Instantiable class)
+Input Validation:
+The text must start with an upper-case letter and end with a full stop. 
+Validation should take place in the instantiable class and will halt the 
+running of the application if validation does not pass
+Scrambling:
+The scrambled text should start with the number of characters in the initial text.
+Replace each vowel with your initials. 
+This should be hard coded into the application and is not required as user input.
+* F1 (Instantiable class, extra method)
+The method should take in 2 parameters; an array of words, and a number. 
+The method should search the array of words and find and output the words matching the given number. 
+If there are multiple words with the same length as the given number, 
+then the method should find all the words that have the same length as that number. 
+The words computed by the method should be stored in an array of words. 
+The method should return the computed array of words.
+*/
+public class TScramblerApp {
+
+}
