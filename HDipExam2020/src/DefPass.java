8b680e54f9c2fefbdcbd4d61b6ed98c70efaf4a2
sev2021 <seweryn.sapinski@gmail.com>
2022-01-05 02:52:38 +0000

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of https://github.com/sev2021/java into main

--- HDipExam2020/src/DefPass.java
+++ HDipExam2020/src/DefPass.java
@@ -2,24 +2,34 @@
 public class DefPass {
 	////INPUT
 	//declare variables
-	private String userInput;
+	private String userInput, userOutput;
 	private int userNo;
 	
 	public DefPass() {
 		userInput = "";
+		userNo = 1;
 	}
 	
 	public void setUserInput(String userInput, int userNo) {
 		this.userInput = userInput;
 		this.userNo = userNo;
-		
+
 	}
 	
 	public void computeUserInput() {
-		
+		userOutput = "";
+		System.out.println(userInput + "-" + userNo); //DEBUG
+		for(int i=0; i<userInput.length(); i++) {
+			if((i + 1)%userNo != 0) {
+				userOutput += userInput.charAt(i);
+			}
+			
+		}
+		userOutput = userOutput.replace(" ", "!");
+		System.out.println(userOutput); //DEBUG
 	}
 	
 	public String getUserInput() {
-		return userInput;
+		return userOutput;
 	}
 }
--- HDipExam2020/src/DefPassApp.java
+++ HDipExam2020/src/DefPassApp.java
@@ -1,14 +1,31 @@
-
+import javax.swing.JOptionPane;
 public class DefPassApp {
+	
 	public static void main(String[] args) {
 		////INPUT
 		//declare vars and objects
-		
+		String userInput;
+		int userNo = 0;
 		DefPass myPass = new DefPass();
 		
-		myPass.setUserInput("Rewe Mtgwetwe", 3);
+		//set
+		userInput = JOptionPane.showInputDialog("Enter name:");
+		do {
+			userNo = Integer.parseInt(JOptionPane.showInputDialog("Select number 3, 4 or 5:"));
+		}
+		while(userNo < 3 || userNo > 5);
+		
+		myPass.setUserInput(userInput, userNo);
+		
+		////PROCESS
+		//compute
 		myPass.computeUserInput();
-		myPass.getUserInput();
+		
+		////OUTPUT
+		//get
+		JOptionPane.showMessageDialog(null, "For name: " + userInput 
+				+ "\ndefault password will be:\n" + myPass.getUserInput());
+		
 		
 	}
 }
