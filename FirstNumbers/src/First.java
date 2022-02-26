8d0c8dbee8bbff1e6ca7a7e7976be57637834368
sev2021 <seweryn.sapinski@gmail.com>
2022-02-26 21:10:00 +0000

    play with First

--- FirstNumbers/src/First.java
+++ FirstNumbers/src/First.java
@@ -1,4 +1,14 @@
-
 public class First {
-
+	//declare variables
+	private int userInput;
+	
+	public First() {}
+	
+	public boolean isFirst(int userInput) {
+		this.userInput = userInput;
+		for(int i = 2; i < (userInput/2); i++) {
+			if(userInput % i == 0) return false;
+		}
+		return true;
+	}
 }
--- FirstNumbers/src/FirstApp.java
+++ FirstNumbers/src/FirstApp.java
@@ -1,4 +1,25 @@
-
+import javax.swing.JOptionPane;
 public class FirstApp {
-
+	public static void main(String[] args) {
+		//declare variable
+		int numb, lastFirst = 2;
+		boolean isFirst;
+		//create object
+		First firstNo = new First();
+		
+		numb = Integer.parseInt(JOptionPane.showInputDialog("Enter number: "));
+		
+		// System.out.println(i + "- " + firstNo.isFirst(numb));
+		
+		for(int i=3; i<numb; i+=2) {
+			
+			if(firstNo.isFirst(i)) {
+				System.out.println(i + " - step - " + (i - lastFirst));
+				lastFirst = i;
+			}
+			
+		}
+		
+		
+	}
 }
